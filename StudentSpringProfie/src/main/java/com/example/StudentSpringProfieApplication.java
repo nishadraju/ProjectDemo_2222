f601aadaa76344c608671ee92659b0d4062c16d6
raju <nishadraju586@gmail.com>
2026-03-07 11:06:12 +0530

    done

---
+++ StudentSpringProfie/src/main/java/com/example/StudentSpringProfieApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringProfieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringProfieApplication.class, args);
+	}
+
+}
---
+++ StudentSpringProfie/src/main/java/com/example/controller.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@Value("${env}")
+	String valueName;
+	
+	@GetMapping("student")
+	public String getName() {
+		
+		
+		return valueName;
+	}
+	
+	
+	
+	
+	
+	
+}
